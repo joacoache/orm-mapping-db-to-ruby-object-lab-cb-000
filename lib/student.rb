48edb5bd71d3f62c2ae73edd6dbc48280d2c0d3c
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-05 01:35:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,15 @@ class Student
       LIMIT ?
     SQL
     DB[:conn].execute(sql, number)
-  end    
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql,)
+  end
 
 end
